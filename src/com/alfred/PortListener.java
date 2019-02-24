e016026a4128b9177b4369e70693006721003e0c
RugaCoder <fruganyumisa@gmail.com>
2019-02-24 19:33:49 +0300

    Integrating with the online repo

--- src/com/alfred/PortListener.java
+++ src/com/alfred/PortListener.java
@@ -1,4 +1,35 @@
 package com.alfred;
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import  java.util.Date;
 public class PortListener {
+
+    public PortListener() {
+
+    }
+
+    public void listener() {
+        //int portNumber = Integer.parseInt(args[0]);
+        int portNumber = 8989;
+        try {
+            ServerSocket serverSocket = new ServerSocket(portNumber);
+            while (true) {
+
+                Socket clientSocket = serverSocket.accept();
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(clientSocket.getInputStream()));
+
+                System.out.println(new Date() + " Incoming Connection Detected");
+                //clientSocket.close();
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+
+    }
 }
